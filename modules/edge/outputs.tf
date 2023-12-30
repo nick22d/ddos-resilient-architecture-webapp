187bab22708d4bb3634a40c4974e2215c82cdd29
ndoropoulos <ndoropoulos@yahoo.com>
2023-12-30 16:37:04 +0000

    Move the CloudFront's domain name to an outputs.tf file in the root module

--- modules/edge/outputs.tf
+++
@@ -1,5 +0,0 @@
-# Output the DNS name of the CloudFront distribution
-output "cloudfront_dns_name" {
-  description = "Return the DNS name of the CloudFront distribution."
-  value       = aws_cloudfront_distribution.distribution.domain_name
-}
