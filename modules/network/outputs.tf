ebe5b423184fd98a9aa2cb3f2e2c5b4557e8f4ae
ndoropoulos <ndoropoulos@yahoo.com>
2023-12-31 18:51:17 +0000

    Output the ALB's DNS name to the root module

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -1,4 +1,4 @@
-# Output the DNS name of the ALB
+# Export the DNS name of the ALB so that it can be output in the root module
 output "alb_dns_name" {
   value = aws_lb.alb.dns_name
 }
--- outputs.tf
+++ outputs.tf
@@ -3,3 +3,9 @@ output "cloudfront_domain" {
   description = "Return the DNS name of the CloudFront distribution."
   value       = module.edge.cloudfront_dns_name
 }
+
+# Output the DNS name of the ALB
+output "alb_dns_name" {
+  description = "Return the DNS name of the ALB."
+  value = module.network.alb_dns_name
+}
