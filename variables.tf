d095c9e7b4ebc75f51b15332c37fe441d5865c39
ndoropoulos <ndoropoulos@yahoo.com>
2023-12-31 21:48:38 +0000

    Add a variable for HTTPs traffic

--- variables.tf
+++ variables.tf
@@ -4,3 +4,10 @@ variable "http_traffic_port" {
   type        = number
   default     = 80
 }
+
+# Define a variable for the HTTPs traffic port
+variable "https_traffic_port" {
+  description = "The main traffic port listening on client HTTPs traffic."
+  type        = number
+  default     = 443
+}
