f2211fda21055e820d10da04b3a42574020b7f11
ndoropoulos <ndoropoulos@yahoo.com>
2024-01-11 15:21:39 +0000

    Add type constraints to all variables.tf files

--- modules/frontend-app/variables.tf
+++ modules/frontend-app/variables.tf
@@ -1,23 +1,23 @@
 # Export the VPC parameter so that it can be populated by the root module
 variable "vpc" {
   description = "The main VPC."
-  type = string
+  type        = string
 }
 
 # Export the 'Security Group for the ALB' parameter so that it can be populated by the root module
 variable "sg_for_alb" {
   description = "The SG for ALB."
-  type = string
+  type        = string
 }
 
 # Export the public subnets so that they can be populated by the root module
 variable "public_subnets" {
   description = "The public subnets."
-  type = string
+  type        = any
 }
 
 # Export the port used for HTTP traffic so that it can be populated by the root module
 variable "http_traffic_port" {
   description = "The port used for HTTP traffic."
-  type = number
+  type        = number
 }
